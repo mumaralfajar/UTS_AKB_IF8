1ec7bbaf5275657a9e4539ad57b0d65146a9be66
mumaralfajar <kuhaaku@gmail.com>
2021-06-06 21:52:04 +0700

    First Commit

---
+++ app/src/main/java/com/example/a10118340_uts_akb_if8/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.a10118340_uts_akb_if8;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView navViewHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navViewHome = findViewById(R.id.nav_view);
+        setupBottomNavigation(savedInstanceState);
+    }
+
+    private void setupBottomNavigation(Bundle savedInstanceState) {
+        BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+                = item -> {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.profile_menu:
+                    fragment = new ProfileFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.notes_menu:
+                    fragment = new NotesFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.about_menu:
+                    fragment = new AboutFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+            }
+            return false;
+        };
+        navViewHome.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        if (savedInstanceState == null) {
+            navViewHome.setSelectedItemId(R.id.profile_menu);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+}
